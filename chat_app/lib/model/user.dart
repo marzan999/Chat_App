c1b23d5f6d2c971b7e824c94cadfc04e936393f5
Marzan Islam <marzanislam999@gmail.com>
2022-10-24 18:18:31 +0600

    .

--- chat_app/lib/model/user.dart
+++ chat_app/lib/model/user.dart
@@ -13,11 +13,10 @@ class User {
     return [
       User(0, 'Marzan', 'Islam', 'images/11.png',
           Color.fromARGB(255, 188, 191, 214)),
-      User(
-          2, 'Alexa', '.', 'images/22.png', Color.fromARGB(255, 175, 160, 197)),
+      User(2, 'Alexa', '', 'images/22.png', Color.fromARGB(255, 175, 160, 197)),
       User(3, 'John', 'Dev', 'images/33.png',
           Color.fromARGB(255, 198, 202, 155)),
-      User(4, 'Lira', '.', 'images/44.png', Color.fromARGB(255, 131, 131, 161)),
+      User(4, 'Lira', '', 'images/44.png', Color.fromARGB(255, 131, 131, 161)),
       User(5, 'Faiza', 'Foxx', 'images/55.png',
           Color.fromARGB(255, 199, 177, 129)),
       User(6, 'Abraham', 'Linkon', 'images/66.png',
--- chat_app/lib/widget/message_widget.dart
+++ chat_app/lib/widget/message_widget.dart
@@ -1,102 +1,69 @@
 // ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 
-//import 'package:chat_app/model/message.dart';
-//import 'package:chat_app/model/message.dart';
+import 'package:chat_app/model/message.dart';
+
 import 'package:chat_app/my_color/color.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
 
 class MessaheWidget extends StatelessWidget {
-  //final messageList = Message.generatedHomePageMessage();
-  //final messageList = Message.generatedHomePageMessage();
+  final messageList = Message.generatedHomePageMessage();
 
   @override
   Widget build(BuildContext context) {
     return Expanded(
       child: Container(
-        // padding: EdgeInsets.symmetric(horizontal: 25, vertical: 40),
+        padding: EdgeInsets.symmetric(horizontal: 25, vertical: 30),
         decoration: BoxDecoration(
             borderRadius: BorderRadius.only(
                 topLeft: Radius.circular(0), topRight: Radius.circular(95)),
             color: Color.fromARGB(255, 162, 177, 180)),
-        child: ListView(
+        child: _buildMessages(),
+      ),
+    );
+  }
+
+  Widget _buildMessages() {
+    return ListView.separated(
+        itemBuilder: (context, index) => _buildMessage(context, index),
+        separatorBuilder: (_, index) => SizedBox(
+              height: 25,
+            ),
+        itemCount: messageList.length);
+  }
+
+  Widget _buildMessage(BuildContext, int index) {
+    return Row(
+      children: [
+        Container(
+          decoration: BoxDecoration(
+            shape: BoxShape.circle,
+            color: messageList[index].user.bgColor,
+          ),
+          child: Image.asset(
+            messageList[index].user.iconUrl,
+            width: 60,
+          ),
+        ),
+        SizedBox(
+          width: 10,
+        ),
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            ListTile(
-              title: Text(
-                'Marzan Islam',
-                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-              ),
-              subtitle: Text('I hate programming'),
-              trailing: Text('10:15'),
-              leading: CircleAvatar(
-                backgroundColor: kPrimaryLightt,
-                backgroundImage: AssetImage('images/11.png'),
-              ),
+            Row(
+              children: [
+                Text(
+                  '${messageList[index].user.firstName} ${messageList[index].user.lastName}',
+                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                )
+              ],
             ),
-            ListTile(
-              title: Text(
-                'Alexa',
-                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-              ),
-              subtitle: Text('Okay done'),
-              trailing: Text('1:10'),
-              leading: CircleAvatar(
-                backgroundColor: kPrimaryDark,
-                backgroundImage: AssetImage('images/22.png'),
-              ),
-            ),
-            ListTile(
-              title: Text(
-                'John Dev',
-                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-              ),
-              subtitle: Text('Aj amar mon valo nei'),
-              trailing: Text('12:45'),
-              leading: CircleAvatar(
-                backgroundColor: kGreyLightt,
-                backgroundImage: AssetImage('images/33.png'),
-              ),
-            ),
-            ListTile(
-              title: Text(
-                'Lira',
-                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-              ),
-              subtitle: Text('Khatam, ta ta, bye bye'),
-              trailing: Text('10:15'),
-              leading: CircleAvatar(
-                backgroundColor: kPrimaryLightt,
-                backgroundImage: AssetImage('images/44.png'),
-              ),
-            ),
-            ListTile(
-              title: Text(
-                'Faiza',
-                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-              ),
-              subtitle: Text('Pera nai, chill'),
-              trailing: Text('10:05'),
-              leading: CircleAvatar(
-                backgroundColor: kPrimary,
-                backgroundImage: AssetImage('images/55.png'),
-              ),
-            ),
-            ListTile(
-              title: Text(
-                'Abraham Linkon',
-                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-              ),
-              subtitle: Text('O vai, maro mujhe maro'),
-              trailing: Text('11:20'),
-              leading: CircleAvatar(
-                backgroundColor: kPrimaryLightt,
-                backgroundImage: AssetImage('images/66.png'),
-              ),
-            ),
+            Text(messageList[index].lastMessage)
           ],
         ),
-      ),
+      ],
     );
   }
 }
